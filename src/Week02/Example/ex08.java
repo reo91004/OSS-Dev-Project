8d0ed19062ed46fc70f86dad4b114e1f027b1970
reo91004 <reo91004@gmail.com>
2021-09-11 02:34:39 +0900

    ~ex15

---
+++ src/Week02/Example/ex08.java
@@ -0,0 +1,8 @@
+package Week02.Example;
+
+public class ex08 {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        System.out.println("두 수의 차는 " + ((a > b) ? (a - b) : (b - a)));
+    }
+}
