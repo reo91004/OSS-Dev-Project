62cd81cd0905ffb106e5a9f771807bd0a737c51a
reo91004 <reo91004@gmail.com>
2021-09-24 16:19:40 +0900

    ex01, ex02

--- src/Week04/Example/Circle.java
+++ src/Week04/Example/Circle.java
@@ -1,32 +1,27 @@
 package Week04.Example;
 
 public class Circle {
-    int radius;
-    String name;
+    int radius; // 원의 반지름 필드
+    String name; // 원의 이름 필드
 
-    public Circle() { // 매개 변수 없는 생성자
-        radius = 1;
-        name = ""; // radius의 초기값은 1
-    }
+    public Circle() {
+    } // 원의 생성자
 
-    public Circle(int r, String n) { // 매개 변수를 가진 생성자
-        radius = r;
-        name = n;
-    }
-
-    public double getArea() {
+    public double getArea() { // 원의 면적 계산 메소드
         return 3.14 * radius * radius;
     }
 
     public static void main(String[] args) {
-        Circle pizza = new Circle(10, "자바피자"); // Circle 객체 생성, 반지름 10
-        double area = pizza.getArea();
-
+        Circle pizza;
+        pizza = new Circle(); // Circle 객체 생성
+        pizza.radius = 10; // 피자의 반지름을 10으로 설정
+        pizza.name = "자바피자"; // 피자의 이름 설정
+        double area = pizza.getArea(); // 피자의 면적 알아내기
         System.out.println(pizza.name + "의 면적은 " + area);
-        Circle donut = new Circle(); // Circle 객체 생성, 반지름 1
-
-        donut.name = "도넛피자";
-        area = donut.getArea();
+        Circle donut = new Circle(); // Circle 객체 생성
+        donut.radius = 2; // 도넛의 반지름을 2로 설정
+        donut.name = "자바도넛"; // 도넛의 이름 설정
+        area = donut.getArea(); // 도넛의 면적 알아내기
         System.out.println(donut.name + "의 면적은 " + area);
     }
 }
