023f473dd09d42375ca34533b9683c9422ab684a
reo91004 <reo91004@gmail.com>
2021-10-01 19:44:46 +0900

    ~Prac02

---
+++ src/Week05/Prac02.java
@@ -0,0 +1,40 @@
+package Week05;
+
+import java.util.Scanner;
+
+class Grade {
+    private int math;
+    private int science;
+    private int english;
+
+    public Grade(int math, int science, int english) {
+        this.math = math;
+        this.science = science;
+        this.english = english;
+    }
+
+    public int average() {
+        return (math + science + english) / 3;
+    }
+
+    public static void sum(int math, int science, int english) {
+        System.out.println("합은 " + (math + science + english));
+    }
+}
+
+public class Prac02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("수학, 과학, 영어 순으로 3개의 점수 입력>>");
+        int math = sc.nextInt();
+        int science = sc.nextInt();
+        int english = sc.nextInt();
+
+        Grade me = new Grade(math, science, english);
+        System.out.println("평균은 " + me.average());
+        Grade.sum(math, science, english);
+
+        sc.close();
+    }
+}
